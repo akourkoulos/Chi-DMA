8c1dba4a32e6acf7017e7ae32778b0b6c8afd8ec
akourkoulos <kourkaggelos@gmail.com>
2022-10-24 23:32:46 +0300

    BS continou

--- CHI-DMA/CHI-DMA.srcs/sources_1/new/BarrelShifter.sv
+++ CHI-DMA/CHI-DMA.srcs/sources_1/new/BarrelShifter.sv
@@ -27,7 +27,7 @@ module BarrelShifter#(
   parameter SHIFT_WIDTH      = 9                     , // log(CHI_DATA_WIDTH*8)
   parameter BRAM_COL_WIDTH   = 32                    ,
   parameter FIFO_LENGTH      = 32                    ,
-  parameter COUNTER_LENGTH   = 6                       // log(FIFO_LENGTH) + 1
+  parameter COUNTER_WIDTH    = 6                       // log(FIFO_LENGTH) + 1
 ) ( 
     input                            RST          ,
     input                            Clk          ,
@@ -39,7 +39,7 @@ module BarrelShifter#(
     input                            EnqueueDst   ,
     input                            RXDATFLITV   ,
     input   DataFlit                 RXDATFLIT    ,
-    output                           RXDATLCRD    ,
+    output                           RXDATLCRDV   ,
     input                            DequeueBS    ,
     output [CHI_DATA_WIDTH*8 - 1 :0] DataOut      ,
     output                           EmptyBS      ,
@@ -52,26 +52,31 @@ module BarrelShifter#(
                         RightShiftState = 2 ,
                         ExtraWriteState = 3  } state , next_state ; 
                         
-    reg  [CHI_DATA_WIDTH*8 - 1 : 0]  PrevShiftedData ; // Register that stores the shifted data that came to the previous DataRsp 
-    wire                             PrvShftdDataWE  ; // WE of register
-    wire [CHI_DATA_WIDTH*8 - 1 : 0]  ShiftedData     ; // Out of Barrel Shifted comb 
+                        
 
-    wire [BRAM_COL_WIDTH   - 1 : 0] AlignedSrcAddr   ;
-    wire [BRAM_COL_WIDTH   - 1 : 0] AlignedDstAddr   ;
-   
-    wire [SHIFT_WIDTH      - 1 : 0] shift            ; // shift of Barrel Shifter
-   
-    wire                            SrcEmpty         ; // SrcAddr FIFO
-    wire                            DeqSrcAddr       ;
-    wire                            SrcAddrLast      ;
-    wire [BRAM_COL_WIDTH   - 1 : 0] SrcAddr          ;
-    wire                            DeqDstAddr       ; // DstAddr FIFO
-    wire                            DstAddrLast      ;
-    wire [BRAM_COL_WIDTH   - 1 : 0] DstAddr          ;
-    wire                            DstEmpty         ;
-    wire                            DeqData          ; // Data FIFO
-    wire [CHI_DATA_WIDTH*8 - 1 : 0] DataFIFO         ; 
-    wire                            DataEmpty        ;
+   reg [`MaxCrds          - 1 : 0] DataCrdInbound   ; // Credits for inbound Data Chanel
+   reg [COUNTER_WIDTH     - 1 : 0] GivenDataCrd     ; // counter used in order not to take more DataRsp than FIFO length
+  
+   reg  [CHI_DATA_WIDTH*8 - 1 : 0]  PrevShiftedData ; // Register that stores the shifted data that came to the previous DataRsp 
+   wire                             PrvShftdDataWE  ; // WE of register
+   wire [CHI_DATA_WIDTH*8 - 1 : 0]  ShiftedData     ; // Out of Barrel Shifted comb 
+
+   wire [BRAM_COL_WIDTH   - 1 : 0] AlignedSrcAddr   ;
+   wire [BRAM_COL_WIDTH   - 1 : 0] AlignedDstAddr   ;
+  
+   wire [SHIFT_WIDTH      - 1 : 0] shift            ; // shift of Barrel Shifter
+  
+   wire                            SrcEmpty         ; // SrcAddr FIFO
+   wire                            DeqSrcAddr       ;
+   wire                            SrcAddrLast      ;
+   wire [BRAM_COL_WIDTH   - 1 : 0] SrcAddr          ;
+   wire                            DeqDstAddr       ; // DstAddr FIFO
+   wire                            DstAddrLast      ;
+   wire [BRAM_COL_WIDTH   - 1 : 0] DstAddr          ;
+   wire                            DstEmpty         ;
+   wire                            DeqData          ; // Data FIFO
+   wire [CHI_DATA_WIDTH*8 - 1 : 0] DataFIFO         ; 
+   wire                            DataEmpty        ;
     
             // SrcAddr FIFO
    FIFO #(  
@@ -179,6 +184,27 @@ module BarrelShifter#(
         end
     end    
     
+    // manage Crds
+     always_ff @ (posedge Clk) begin
+     if(RST)begin
+       DataCrdInbound <= 0 ;        // Reset FSM
+       GivenDataCrd   <= 0 ;
+     end        
+     else begin
+       // Inbound Data chanle Crd Counter
+       if(RXDATLCRDV & !(DataCrdInbound != 0 & RXDATFLITV))
+         DataCrdInbound <= DataCrdInbound + 1 ;
+       else if(!RXDATLCRDV & (DataCrdInbound != 0 & RXDATFLITV))
+         DataCrdInbound <= DataCrdInbound - 1 ;
+       // Count the number of given Data Crds in order not to give more than DATA FIFO length
+       if(RXDATLCRDV & !DequeueBS)
+         GivenDataCrd <= GivenDataCrd + 1 ;       
+       else if(!RXDATLCRDV & DequeueBS)
+         GivenDataCrd <= GivenDataCrd - 1 ;
+     end
+   end
+   // Give an extra Crd in outbound Data Chanel
+   assign RXDATLCRDV = !RST & (GivenDataCrd < FIFO_LENGTH & DataCrdInbound < `MaxCrds) ;
     
      //FSM's state
    always_ff @ (posedge Clk) begin
@@ -187,58 +213,44 @@ module BarrelShifter#(
      else
        state <= next_state ;        // Change state
    end
-   
-   reg [CrdRegWidth - 1 : 0 ] DataCrdInbound;
-   reg
-    always_ff @ (posedge Clk) begin
-     if(RST)
-       state <= StartState ;        // Reset FSM
-     else begin
-       if(RXDATLCRDV & !(DataCrdInbound != 0 & RXDATFLITV))
-         DataCrdInbound <= DataCrdInbound + 1 ;
-       else if(!RXDATLCRDV & (DataCrdInbound != 0 & RXDATFLITV))
-         DataCrdInbound <= DataCrdInbound - 1 ;
-       // Count the number of given Data Crds in order not to give more than DATA FIFO length
-       if(RXDATLCRDV & !SigDeqData)
-         GivenDataCrd <= GivenDataCrd + 1 ;       
-       else if(!RXDATLCRDV & SigDeqData)
-         GivenDataCrd <= GivenDataCrd - 1 ;
-     end
-   end
-    
          
    //FSM's next_state
    always_comb begin : next_state_logic
      case(state)
        StartState :
          begin                           
-           if(!FIFOEmpty & !LastDstTrans & !LastSrcTrans & RXDATFLITV & (DstAddr - AlignedDstAddr) > (SrcAddr - AlignedSrcAddr))      // When a valid DataRsp came and DstAddr is bigger than SrcAddr compare to their aligned Addresses then Data must be shifted left  
+           if(!EmptyBS & !LastDstTrans & !LastSrcTrans & DequeueBS & (DstAddr - AlignedDstAddr) > (SrcAddr - AlignedSrcAddr))      // When DstAddr is bigger than SrcAddr compare to their aligned Addresses then Data must be shifted left  
              next_state = LeftShiftState ;
-           else if(!FIFOEmpty & !LastSrcTrans & RXDATFLITV & (DstAddr - AlignedDstAddr) < (SrcAddr - AlignedSrcAddr))                 // When a valid DataRsp came and DstAddr is smaller than SrcAddr compare to their aligned Addresses then Data must be shifted right  
+           else if(!EmptyBS & !LastSrcTrans & DequeueBS & (DstAddr - AlignedDstAddr) < (SrcAddr - AlignedSrcAddr))                 // When DstAddr is smaller than SrcAddr compare to their aligned Addresses then Data must be shifted right  
              next_state = RightShiftState ;
-           else if(!FIFOEmpty & !LastDstTrans & LastSrcTrans & RXDATFLITV & (DstAddr - AlignedDstAddr) > (SrcAddr - AlignedSrcAddr))  // When a valid DataRsp came and it is the last Read Trans but not the last Write then should be an extra write  
+           else if(!EmptyBS & !LastDstTrans & LastSrcTrans & DequeueBS & (DstAddr - AlignedDstAddr) > (SrcAddr - AlignedSrcAddr))  // When it is the last Read Trans but not the last Write then should be an extra write  
              next_state = ExtraWriteState ;
+           else
+             next_state = StartState ; 
          end
        LeftShiftState :
          begin
-           if((DescDataIn.BytesToSend == DescDataOut.SentBytes & !CmdFIFOFULL) | !ReadyBRAM) // All Bytes of a Descriptor have been scheduled
-             next_state = IdleState ;
-           else if (!CmdFIFOFULL & ReadyBRAM)     // A chunk of one Descriptor have been served
-             next_state = WriteBackState ;
+           if(LastSrcTrans & LastDstTrans & !EmptyBS & DequeueBS)         //  When last Read becomes the last Write return to StartState
+             next_state = StartState ;
+           else if (LastSrcTrans & !LastDstTrans & !EmptyBS & DequeueBS)  //  When last Read completes a non-last Write then it should complete the last Write as well so an extra write should be done 
+             next_state = ExtraWriteState ;
            else
-             next_state = IssueState ;            // Chunk is not over and there are more Bytes of the current Descriptor to be sent 
+             next_state = LeftShiftState ;            
          end
        RightShiftState : 
          begin
-           if(ReadyFIFO & ReadyBRAM)              // Descriptor Addr pointer wrote back to FIFO and there is Control of BRAM to read next Descriptor                                            
-             next_state = IssueState ;
-           else if (ReadyFIFO & !ReadyBRAM)       // Descriptor Addr pointer wrote back to FIFO and there is not Control of BRAM to read next Descriptor                                        
-             next_state = IdleState ;
+           if(LastSrcTrans & LastDstTrans & !EmptyBS & DequeueBS)       //  When last Read becomes the last Write return to StartState                
+             next_state = StartState ; 
+           else if (LastSrcTrans & !LastDstTrans & !EmptyBS & DequeueBS) //  When last Read completes a non-last Write then it should complete the last Write as well so an extra write should be done                                        
+             next_state = ExtraWriteState ;
            else
-             next_state = WriteBackState ;        // FIFO's control has not obtained yet                                                                                                            
+             next_state = RightShiftState ;                                                                                                                    
          end 
        ExtraWriteState : 
-         next_state = StartState ;
+         if(LastSrcTrans & LastDstTrans & !EmptyBS & DequeueBS)  //  When last Read and last Write return to StartState
+           next_state = StartState ;
+         else
+           ExtraWriteState ;
        default : next_state = StartState ;          // Default state
      endcase   
    end
